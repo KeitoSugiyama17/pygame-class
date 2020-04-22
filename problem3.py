434cdc9e07033910535828360f2cd1cb46fae725
keito <ksugiyama017@gmail.com>
2020-04-22 15:25:39 -0700

    Homework for lesson 2

---
+++ problem3.py
@@ -0,0 +1,12 @@
+def print_rectangle(w, h):
+    for i in range(0, h):
+        line = ''
+        if (i == 0) or (i == h - 1):
+            for j in range(0, w):
+                line = line + '*'
+        else:
+            line = '*'
+            for j in range(0, w - 2):
+                line = line + ' '
+            line = line + '*'
+        print(line)
